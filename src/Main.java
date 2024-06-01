dc10bc3a53914029122fc16b28823cae6053bf66
yiuki <butenko.vlad97@gmail.com>
2024-06-01 22:23:43 +0300

    correcting Runnable class

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,33 @@
 
-
-import javax.swing.plaf.TableHeaderUI;
 import java.util.Scanner;
 
 public class Main {
+
+    //release Threads lessons
+
     public static void main(String[] args) {
         System.out.println("Введите число элементов до 10");
-/*      new ElementsList(new Scanner(System.in).nextInt()).start();
+/*
+        //release with additional Class
+        new ElementsList(new Scanner(System.in).nextInt()).start();
         new ElementsList(new Scanner(System.in).nextInt()).start();
         new ElementsList(new Scanner(System.in).nextInt()).start();
         new ElementsList(new Scanner(System.in).nextInt()).start();
         new ElementsList(new Scanner(System.in).nextInt()).start();*/
 
-/*      new Thread(new NewTest(new Scanner(System.in).nextInt())).start();
+        //Thread class release
         new Thread(new NewTest(new Scanner(System.in).nextInt())).start();
-        new Thread(new NewTest(new Scanner(System.in).nextInt())).start();*/
+        new Thread(new NewTest(new Scanner(System.in).nextInt())).start();
+        new Thread(new NewTest(new Scanner(System.in).nextInt())).start();
+
+        //release with Runnable
 
         new Thread(new Runnable() {
 
             @Override
             public void run() {
                 int i = new Scanner(System.in).nextInt();
-                for (i = i; i < 10; i++) {
+                for (; i <= 10; i++) {
                     System.out.println("Элемент номер" + " " + i);
                 }
             }
