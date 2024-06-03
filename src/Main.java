42b30028ddaf519730817a161eb5db7e0e127436
yiuki <butenko.vlad97@gmail.com>
2024-06-04 00:02:08 +0300

    Leetcode solution

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,10 @@
 import java.math.BigInteger;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-
-        long startTime = System.currentTimeMillis();
-
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                BigInteger factorial = new BigInteger(String.valueOf(1));
-                for (int i = 1; i <= 300000; i++) {
-                    factorial = factorial.multiply(BigInteger.valueOf(i));
-                }
-                System.out.println(factorial);
-            }
-        });
-        t1.start();
-        while (t1.isAlive()) {
-            System.out.println("Падажжи");
-            Thread.sleep(1000);
-        }
-
-
-        long endTime = System.currentTimeMillis();
-        System.out.println(endTime-startTime);
+        int[] res = {3,9,7,2};
+        System.out.println(Arrays.toString(new Solution().twoSum(res, 16)));
     }
 }
