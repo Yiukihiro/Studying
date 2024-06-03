f465d56e1043ba3938100f3d0eba9cfc19625c01
yiuki <butenko.vlad97@gmail.com>
2024-06-03 22:08:41 +0300

    Thread program for calculating factorial
    do it correctly

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,29 @@
-import java.util.Scanner;
+import java.math.BigInteger;
 
 public class Main {
 
-
     public static void main(String[] args) throws InterruptedException {
-        new CreateCharacter(StartTheDialog.startTheDialog()).createCharacter();
-        String answer = new Scanner(System.in).nextLine();
-        switch (answer) {
-            case "Нет": new PhasesList(0).phrases();
-            break;
-            case "Да": new PhasesList(1).phrases();
-            break;
+
+        long startTime = System.currentTimeMillis();
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                BigInteger factorial = new BigInteger(String.valueOf(1));
+                for (int i = 1; i <= 300000; i++) {
+                    factorial = factorial.multiply(BigInteger.valueOf(i));
+                }
+                System.out.println(factorial);
+            }
+        });
+        t1.start();
+        while (t1.isAlive()) {
+            System.out.println("Падажжи");
+            Thread.sleep(1000);
         }
 
+
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime-startTime);
     }
 }
