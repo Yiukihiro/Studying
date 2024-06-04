ac515001bbd61245bb7296151822bc46ef680f80
yiuki <butenko.vlad97@gmail.com>
2024-06-04 23:15:29 +0300

    Leetcode question: The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
    Solve with bad case for algorithm of leetcode.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
     public static void main(String[] args)  {
-        int[] nums = {1,2,1,1,3,3,3,1,1};
+        int[] nums = {1,1,1,3,3,3,1};
         System.out.println(new Solution().majorityElement(nums));
     }
 }
