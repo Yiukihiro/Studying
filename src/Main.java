43007169771df84b406fdb9e40c1930395e39572
yiuki <butenko.vlad97@gmail.com>
2024-06-06 13:35:28 +0300

    Leetcode question: Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome  that can be built with those letters.
    Letters are case sensitive, for example, "Aa" is not considered a palindrome
    
    Start

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@
 public class Main {
 
     public static void main(String[] args)  {
-        int[] nums = {1,1,1,3,3,3,1};
-        System.out.println(new Solution().majorityElement(nums));
+        System.out.println(new Solution().longestPalindrome("aaAbbBccc"));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,11 @@
+import java.util.Arrays;
+
 class Solution {
-    public int majorityElement(int[] nums) {
-        int value = -1;
-        int checkRank = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (checkRank == 0) {
-                value = nums[i];
-                checkRank = 1;
-            } else if (value == nums[i]) {
-                checkRank++;
-                if (checkRank > (nums.length / 2)) {
-                    return value;
-                }
-            } else checkRank--;
-        }
-        return value;
+    public int longestPalindrome(String s) {
+        char[] chars = s.toLowerCase().toCharArray();
+        int n = chars.length;
+        System.out.println(chars);
+
+        return n;
     }
 }
